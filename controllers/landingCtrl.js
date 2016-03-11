9142ee6dc33b13fc68577b5fc9105483e0871c93
Corey Robison <coreybrobison@gmail.com>
2016-03-11 13:42:28 -0700

    made more changes to landing template

--- controllers/landingCtrl.js
+++ controllers/landingCtrl.js
@@ -1,11 +1,17 @@
 angular.module("codeLibrary").controller('landingCtrl', function($scope, landingService) {
 
 
-   
+
 
     $(document).ready(function() {
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
+    });
+
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
     });
 
 
